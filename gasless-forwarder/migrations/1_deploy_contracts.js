437c6a2cc5994d4f7dc3d8b6bfe291a1ddd8ef27
exploring-solver <amansharma12607@gmail.com>
2025-02-21 21:21:38 +0530

    Cleanup

--- gasless-forwarder/migrations/1_deploy_contracts.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (deployer) {
-  deployer.deploy(TestToken, 1000000); // Deploy TestToken with an initial supply of 1,000,000 tokens
-  deployer.deploy(Forwarder); // Deploy the Forwarder contract
-};
